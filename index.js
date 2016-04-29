a4c75be67680bd0fd96b7f7420a5ace1d573952a
Allan Armstrong <allan@hit3k.net>
2016-04-29 12:52:15 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.set ('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/api/whoami', function(req, res) {
+
+	var ip = req.ip;
+	var language = req.acceptsLanguages()[0];
+	var UA = req.header('user-agent').split('(')[1].split(')')[0];
+	var response = {ipaddress: ip, language: language, software: UA};
+	res.json(response);
+});
+
+app.get('/', function(req, res) {
+	res.redirect('/api/whoami');
+});
+
+app.listen(app.get('port'), function() {
+	console.log("Listening on port: " + app.get('port'));
+})
+
